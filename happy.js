5adc8e9b85745ff1a90edc10e8abc5c47167f7f1
Hardik Amarwani <82609732+Hard-Hustler@users.noreply.github.com>
2023-07-07 02:37:03 +0530

    Update happy.js

--- happy.js
+++ happy.js
@@ -53,9 +53,6 @@ const happpy=()=>{
           <h2 class="text-white text-lg title-font font-medium mb-2">Neptune</h2>
           <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard ugh iceland kickstarter tumblr live-edge tilde.</p>
           <a class="mt-3 text-purple-400 inline-flex items-center">Learn More
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
           </a>
         </div>
       </div>
